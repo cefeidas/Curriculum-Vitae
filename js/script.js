b52f566c969cffdf9dfd297356ffa364683afc81
cefeidas <danmarmon1986@gmail.com>
2024-01-24 13:48:21 +0000

    First squeleton of the project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Funcionalidad de traducción
+document.getElementById('btn-es').addEventListener('click', () => {
+    // Cambiar el contenido al español
+});
+
+document.getElementById('btn-en').addEventListener('click', () => {
+    // Cambiar el contenido al inglés
+});
+
+// Aquí puedes añadir más interactividad con JavaScript
